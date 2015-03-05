44eab3a5d9d8dde915ca8c0b2d3285b1ca33551c
Micah J. Obal <micah_obal@yahoo.com>
2015-03-05 17:16:06 -0600

    Very unfinished with euler003...

--- Javascript-101/euler003.js
+++ Javascript-101/euler003.js
@@ -1,6 +1,13 @@
+/*
+Very unfinished! Currently just a prime generator. I'm not sure if I'm barking up the right tree!
+*/
+
 var target = 1000;
+
+/*I save a bit of headache by not starting primeList or iterator at 0 or 1 here.*/
 var primeList = [2];
 var iterator = 3;
+
 var isPrime = true;
 
 while(iterator <= target){
@@ -8,13 +15,13 @@ while(iterator <= target){
 	for(var i = 0; i < primeList.length; i++){
 		if(iterator % primeList[i] === 0){
 			isPrime = false;
-			i = primeList.length;
+			i = primeList.length; /*Should I just use a 'break' command here?*/
 		}
 	}
 	if (isPrime === true){
 		primeList.push(iterator);
 	}
-	iterator += 2;
+	iterator += 2; /*Even numbers can't be prime...*/
 }
 
 console.log(primeList);
