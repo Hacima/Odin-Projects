a955b116b7b610590cae18d3860aa675f0466239
Micah J. Obal <micah_obal@yahoo.com>
2015-04-06 17:04:07 -0500

    Added color selection, reset button and a randomize button. Haven't run into any serious challenges yet.

--- Sketchpad/script.js
+++ Sketchpad/script.js
@@ -2,12 +2,32 @@ $(document).ready(function() {
     var pixelAreaSize = 500,
         pixelSize = 20,
         pixelsPerLine = pixelAreaSize/pixelSize,
+        selectedColor = "black",
         i = 0;
     for (i = 0; i < (pixelsPerLine*pixelsPerLine); i += 1) {
         $("#pixelArea").append("<div class='pixel'></div>");
-    };
+    }
     
-    $(".pixel").click(function(){
-        $(this).toggleClass("grey");
-    })
-})
+    $(".color").click(function() {
+        selectedColor = $(this).css("background-color");
+        $(".color").css("border", "1px solid black"); //reset borders
+        $(this).css("border", "2px solid black"); //add 'selection' border to selected color
+    });
+    
+    $(".pixel").click(function() {
+        $(this).css("background-color", selectedColor);
+    });
+    
+    $("#reset").click(function() {
+        $(".pixel").css("background-color", "black");
+    });
+    
+    $("#randomize").click(function() {
+        $(".pixel").each(function() {
+            var randomOne = Math.floor(Math.random() * 256),
+                randomTwo = Math.floor(Math.random() * 256),
+                randomThree = Math.floor(Math.random() * 256);
+            $(this).css("background-color", "rgb(" + randomOne + ',' + randomTwo + ',' + randomThree + ')');
+        });
+    });
+});
