c1628aa914b94f3dad3527e85cfba50fbadc4f5f
Micah J. Obal <micah_obal@yahoo.com>
2015-04-06 17:44:49 -0500

    Added the ability to drag your selected color across the sketchpad. Also changed the 'pixels' from circles to boxes since it looks way better. Did a tiny bit of styling too.

--- Sketchpad/script.js
+++ Sketchpad/script.js
@@ -3,19 +3,34 @@ $(document).ready(function() {
         pixelSize = 20,
         pixelsPerLine = pixelAreaSize/pixelSize,
         selectedColor = "black",
+        isDown = false;
         i = 0;
     for (i = 0; i < (pixelsPerLine*pixelsPerLine); i += 1) {
         $("#pixelArea").append("<div class='pixel'></div>");
     }
     
+    $(document).mousedown(function() {
+        isDown = true;      // When mouse goes down, set isDown to true
+    })
+    .mouseup(function() {
+        isDown = false;    // When mouse goes up, set isDown to false
+    });
+
+    
     $(".color").click(function() {
         selectedColor = $(this).css("background-color");
-        $(".color").css("border", "1px solid black"); //reset borders
-        $(this).css("border", "2px solid black"); //add 'selection' border to selected color
+        $(".color").css("border", "0px solid black"); //reset borders
+        $(this).css("border", "1px solid black"); //add 'selection' border to selected color
     });
     
     $(".pixel").click(function() {
         $(this).css("background-color", selectedColor);
+    });
+    
+    $(".pixel").mouseover(function() {
+        if(isDown) {
+            $(this).css("background-color", selectedColor);
+        }
     });
     
     $("#reset").click(function() {
