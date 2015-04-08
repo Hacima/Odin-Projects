54461b2c8c80ddd53a9ced3ff6e0fb60ebe9b812
Micah J. Obal <micah_obal@yahoo.com>
2015-04-08 01:34:20 -0500

    Added gradient functionality. The only thing left to do is add a random paintbrush!

--- Sketchpad/sketchpad.js
+++ Sketchpad/sketchpad.js
@@ -1,15 +1,10 @@
 $(document).ready(function() {
-    var pixelAreaSize = 500,
-        pixelSize = 20,
-        pixelsPerLine = pixelAreaSize/pixelSize,
-        selectedColor = "black",
+    var selectedColor = "black",
         isDown = false;
-        i = 0;
     
-    for (i = 0; i < (pixelsPerLine*pixelsPerLine); i += 1) {
-        $("#pixelArea").append("<div class='pixel'></div>");
-    }
-    
+    //Initializing the sketchpad.
+    resize(20);
+
     //Color selection
     $(".color").click(function() {
         selectedColor = $(this).css("background-color");
@@ -27,16 +22,25 @@ $(document).ready(function() {
     
     $('#pixelArea').on('mouseover', 'div', function() {
         if(isDown) {
-            $(this).css("background-color", selectedColor);
+            if($("#gradient").prop("checked")) {
+                gradient($(this));
+            } else {
+                $(this).css("background-color", selectedColor);
+            }
         }
     });
     
     //This might seem redundant, but it serves to paint the pixel that the cursor is hovering over
     //upon click instead of waiting for it to release the click.
     $('#pixelArea').on('mousedown', 'div', function() {
-        $(this).css("background-color", selectedColor);
+        if($("#gradient").prop("checked")) {
+            gradient($(this));
+        } else {
+            $(this).css("background-color", selectedColor);
+        }
     });
     
+    //Just changes every pixel to black. Super simple
     $("#reset").click(function() {
         $(".pixel").css("background-color", "black");
     });
@@ -46,14 +50,16 @@ $(document).ready(function() {
     });
     
     $("#resize").click(function() {
-        resize(prompt("How many pixels would you like per line? (numbers above 100 are very slow - be warned!)"));
+        var pixelsPerLine = prompt("How many pixels would you like per line? (numbers above 100 are very slow - be warned!)")
+        resize(pixelsPerLine);
     });
 });
 
-function resize(newSize) {
-    var numPixels = newSize * newSize; 
-    var pixelSize = 500 / newSize; //500px is how wide our container is. Magic number, I know. =/
-    var i = 0;
+function resize(pixelsPerLine) {
+    var numPixels = pixelsPerLine * pixelsPerLine,
+        canvasWidth = parseInt($("#pixelArea").css("width")),
+        pixelSize = canvasWidth / pixelsPerLine,
+        i = 0;
     
     //Get rid of the old pixels
     $("#pixelArea").empty();
@@ -64,10 +70,10 @@ function resize(newSize) {
     }
     
     //Set the pixels to their new size. They will always be squares, so the height and width are the same.
-    $(".pixel").css("width", pixelSize);
-    $(".pixel").css("height", pixelSize);
+    $(".pixel").css({"width": pixelSize, "height": pixelSize});
 }
 
+//Super basic! For each pixel, three random numbers from 0 to 255 are generated and used in rgb()!
 function randomize() {
     $(".pixel").each(function() {
         var randomOne = Math.floor(Math.random() * 256),
@@ -76,4 +82,25 @@ function randomize() {
             
         $(this).css("background-color", "rgb(" + randomOne + ',' + randomTwo + ',' + randomThree + ')');
     });
+}
+
+function gradient($pixel) {
+    //Get the pixels rgb values. rgb = "rgb(r,g,b)" currently. We only want r, g and b, though...
+    var rgb = $pixel.css("background-color");
+    
+    //This expression gets the values of x. Now rgb = ["r", "g", "b"]. They are still strings.
+    rgb = rgb.match(/\d+/g);
+
+    //This loop converts rbg[i] into an int, then decreases it by 10, making sure it doesn't go below 0.
+    for (var i in rgb) {
+        rgb[i] = parseInt(rgb[i]);
+        if (rgb[i] <= 10 && rgb[i] > 0) {
+            rgb[i] = 0;
+        } else if (rgb[i] > 10) {
+            rgb[i] -= 10;
+        }
+    }
+    
+    //And finally we plug 'em in! Voila, a very slight gradient is accomplished.
+    $pixel.css("background-color", "rgb(" + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')');
 }
